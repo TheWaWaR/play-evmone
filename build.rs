f9d488280c2a2bc9f24e0618e079792674030696
Linfeng Qian <thewawar@gmail.com>
2020-04-29 20:01:25 +0800

    First Commit

---
+++ build.rs
@@ -0,0 +1,38 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let libevmone_file = "libevmone.so";
+    if let Some(path) = first_path_with_file(libevmone_file) {
+        println!("cargo:rustc-link-search=native={}", path);
+        println!("cargo:rustc-link-lib=dylib=evmone");
+    } else {
+        panic!("Can not find {}", libevmone_file);
+    }
+}
+
+fn first_path_with_file(file: &str) -> Option<String> {
+    // we want to look in LD_LIBRARY_PATH and then some default folders
+    if let Some(ld_path) = env::var_os("LD_LIBRARY_PATH") {
+        for p in env::split_paths(&ld_path) {
+            if is_file_in(file, &p) {
+                return p.to_str().map(String::from)
+            }
+        }
+    }
+    for p in &["/usr/lib","/usr/local/lib"] {
+        if is_file_in(file, &Path::new(p)) {
+            return Some(String::from(*p))
+        }
+    }
+    None
+}
+
+fn is_file_in(file: &str, folder: &Path) -> bool {
+    let full = folder.join(file);
+    match fs::metadata(full) {
+        Ok(ref found) if found.is_file() => true,
+        _ => false
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,118 @@
+// use libc;
+use std::collections::HashMap;
+use std::alloc::{dealloc, Layout};
+use std::ptr;
+
+use evmc_vm::{
+    self as vm,
+    ffi,
+    EvmcVm,
+    EvmcContainer,
+    ExecutionContext,
+    ExecutionMessage,
+    ExecutionResult,
+};
+
+
+#[link(name = "evmone")]
+extern {
+    fn evmc_create_evmone() -> *mut ffi::evmc_vm;
+}
+
+fn main() {
+    for i in 0..3 {
+        let host = ffi::evmc_host_interface {
+            account_exists: None,
+            get_storage: None,
+            set_storage: None,
+            get_balance: None,
+            get_code_size: None,
+            get_code_hash: None,
+            copy_code: None,
+            selfdestruct: None,
+            call: None,
+            get_tx_context: Some(get_dummy_tx_context),
+            get_block_hash: None,
+            emit_log: None,
+        };
+        let host_context = Box::new(HostContext::default());
+        let host_context_ptr = Box::into_raw(host_context) as *mut ffi::evmc_host_context;
+        let mut context = ExecutionContext::new(&host, host_context_ptr);
+        let evmone = unsafe { evmc_create_evmone() };
+        let container: Box<EvmcContainer<TestVm>> = unsafe {
+            EvmcContainer::from_ffi_pointer(evmone)
+        };
+
+        let message = vm::ExecutionMessage::new(
+            vm::MessageKind::EVMC_CALL,
+            0,
+            0,
+            0,
+            vm::Address::default(),
+            vm::Address::default(),
+            None,
+            vm::Uint256::default(),
+            vm::Bytes32::default(),
+        );
+        let code = [0u8; 0];
+        let result = container
+            .execute(
+                vm::Revision::EVMC_PETERSBURG,
+                &code,
+                &message,
+                Some(&mut context)
+            );
+        println!("[Round {}] Execution result: {:?}", i, result);
+
+        unsafe {
+            // Otherwise host_context will leak memory
+            ptr::drop_in_place(host_context_ptr);
+            dealloc(host_context_ptr as *mut u8, Layout::new::<HostContext>());
+        }
+    }
+}
+
+struct TestVm {}
+
+impl EvmcVm for TestVm {
+    fn init() -> Self {
+        TestVm {}
+    }
+
+    fn execute(
+        &self,
+        _revision: ffi::evmc_revision,
+        _code: &[u8],
+        message: &ExecutionMessage,
+        _context: Option<&mut ExecutionContext>,
+    ) -> ExecutionResult {
+        println!("message: {:?}", message);
+        ExecutionResult::failure()
+    }
+}
+
+type Bytes32 = [u8; 32];
+
+#[derive(Default, Debug)]
+pub struct HostContext {
+    code: Vec<u8>,
+    storage: HashMap<Bytes32, Bytes32>,
+}
+
+
+unsafe extern "C" fn get_dummy_tx_context(
+    _context: *mut ffi::evmc_host_context,
+) -> ffi::evmc_tx_context {
+    // let ctx = Box::from_raw(context as *mut HostContext);
+    // println!("host context: {:?}", ctx);
+    ffi::evmc_tx_context {
+        tx_gas_price: vm::Uint256::default(),
+        tx_origin: vm::Address::default(),
+        block_coinbase: vm::Address::default(),
+        block_number: 0,
+        block_timestamp: 0,
+        block_gas_limit: 0,
+        block_difficulty: vm::Uint256::default(),
+        chain_id: vm::Uint256::default(),
+    }
+}

a2f7137132e05ba6d808c2893ff169c4c1d1d6ab
Linfeng Qian <thewawar@gmail.com>
2020-04-30 19:01:10 +0800

    Expose all callback interface

--- src/evmone.rs
+++ src/evmone.rs
@@ -1,9 +1,25 @@
 use std::alloc::{dealloc, Layout};
-use std::collections::HashMap;
-use std::ops::Deref;
+use std::marker::PhantomData;
+use std::ops::{Deref, DerefMut};
 use std::ptr;
+use std::slice::from_raw_parts;
 
-use evmc_vm::ffi;
+use evmc_sys as ffi;
+
+/// EVMC call kind.
+pub type CallKind = ffi::evmc_call_kind;
+
+/// EVMC message (call) flags.
+pub type CallFlags = ffi::evmc_flags;
+
+/// EVMC status code.
+pub type StatusCode = ffi::evmc_status_code;
+
+/// EVMC storage status.
+pub type StorageStatus = ffi::evmc_storage_status;
+
+/// EVMC VM revision.
+pub type Revision = ffi::evmc_revision;
 
 #[link(name = "evmone")]
 extern "C" {
@@ -22,7 +38,7 @@ impl Evmone {
 
     pub fn execute(
         &self,
-        revision: ffi::evmc_revision,
+        revision: Revision,
         code: &[u8],
         message: &ExecutionMessage,
         context: &mut ExecutionContext,
@@ -43,8 +59,34 @@ impl Evmone {
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Hash)]
-pub struct Bytes32(pub [u8; 32]);
+macro_rules! impl_convert {
+    ($struct:ident, $inner:ty, $target:path) => {
+        #[derive(Eq, PartialEq, Hash, Default, Clone)]
+        pub struct $struct(pub $inner);
+
+        impl From<$target> for $struct {
+            fn from(data: $target) -> $struct {
+                $struct(data.bytes)
+            }
+        }
+        impl From<$struct> for $target {
+            fn from(data: $struct) -> $target {
+                $target { bytes: data.0 }
+            }
+        }
+        impl ::std::fmt::Debug for $struct {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
+                let prefix = if f.alternate() { "0x" } else { "" };
+                write!(f, "{}{}", prefix, ::hex::encode(self.0))
+            }
+        }
+    };
+}
+
+impl_convert!(Address, [u8; 20], ffi::evmc_address);
+impl_convert!(Bytes32, [u8; 32], ffi::evmc_bytes32);
+// Big Endian
+impl_convert!(Uint256, [u8; 32], ffi::evmc_uint256be);
 
 #[derive(Debug)]
 pub struct ExecutionResult {
@@ -54,6 +96,11 @@ pub struct ExecutionResult {
 impl From<ffi::evmc_result> for ExecutionResult {
     fn from(result: ffi::evmc_result) -> ExecutionResult {
         ExecutionResult { inner: result }
+    }
+}
+impl From<ExecutionResult> for ffi::evmc_result {
+    fn from(result: ExecutionResult) -> ffi::evmc_result {
+        result.inner
     }
 }
 
@@ -84,86 +131,233 @@ impl ExecutionContext {
     }
 }
 
-pub struct ExecutionMessage {
-    pub inner: ffi::evmc_message,
+#[derive(Debug)]
+pub struct ExecutionMessage<'a> {
+    pub inner: &'a ffi::evmc_message,
 }
 
-impl From<ffi::evmc_message> for ExecutionMessage {
-    fn from(inner: ffi::evmc_message) -> ExecutionMessage {
+impl<'a> From<&'a ffi::evmc_message> for ExecutionMessage<'a> {
+    fn from(inner: &'a ffi::evmc_message) -> ExecutionMessage<'a> {
         ExecutionMessage { inner }
     }
 }
 
-impl ExecutionMessage {
-    pub fn as_ptr(&self) -> *const ffi::evmc_message {
-        (&self.inner) as *const ffi::evmc_message
+impl<'a> Deref for ExecutionMessage<'a> {
+    type Target = ffi::evmc_message;
+    fn deref(&self) -> &Self::Target {
+        self.inner
     }
 }
 
-#[derive(Default, Debug)]
-pub struct HostContext {
-    pub code: Vec<u8>,
-    pub storage: HashMap<Bytes32, Bytes32>,
+impl<'a> ExecutionMessage<'a> {
+    pub fn as_ptr(&self) -> *const ffi::evmc_message {
+        self.inner as *const ffi::evmc_message
+    }
 }
 
-pub struct HostContextPtr {
+pub struct HostContextPtr<T> {
     pub ptr: *mut ffi::evmc_host_context,
+    _data: PhantomData<T>,
 }
 
-impl Drop for HostContextPtr {
+impl<T: Sized> Drop for HostContextPtr<T> {
     fn drop(&mut self) {
         unsafe {
             ptr::drop_in_place(self.ptr);
-            dealloc(self.ptr as *mut u8, Layout::new::<HostContext>());
+            dealloc(self.ptr as *mut u8, Layout::new::<T>());
         }
     }
 }
 
-impl From<Box<HostContext>> for HostContextPtr {
-    fn from(ctx: Box<HostContext>) -> HostContextPtr {
+impl<T: HostContext + Sized> From<Box<T>> for HostContextPtr<T> {
+    fn from(ctx: Box<T>) -> HostContextPtr<T> {
         let ptr = Box::into_raw(ctx) as *mut ffi::evmc_host_context;
-        HostContextPtr { ptr }
+        HostContextPtr {
+            ptr,
+            _data: PhantomData,
+        }
     }
 }
 
-pub struct HostContextWrapper {
-    inner: Option<Box<HostContext>>,
+pub struct HostContextWrapper<T> {
+    inner: Option<Box<T>>,
 }
 
-impl Drop for HostContextWrapper {
+impl<T> Drop for HostContextWrapper<T> {
     fn drop(&mut self) {
         std::mem::forget(self.inner.take().unwrap());
     }
 }
 
-impl From<*mut ffi::evmc_host_context> for HostContextWrapper {
-    fn from(ptr: *mut ffi::evmc_host_context) -> HostContextWrapper {
-        let inner = Some(unsafe { Box::from_raw(ptr as *mut HostContext) });
+impl<T: HostContext> From<*mut ffi::evmc_host_context> for HostContextWrapper<T> {
+    fn from(ptr: *mut ffi::evmc_host_context) -> HostContextWrapper<T> {
+        let inner = Some(unsafe { Box::from_raw(ptr as *mut T) });
         HostContextWrapper { inner }
     }
 }
 
-impl Deref for HostContextWrapper {
-    type Target = Box<HostContext>;
+impl<T: HostContext> Deref for HostContextWrapper<T> {
+    type Target = T;
     fn deref(&self) -> &Self::Target {
         self.inner.as_ref().unwrap()
     }
 }
 
-pub unsafe extern "C" fn get_dummy_tx_context(
-    context: *mut ffi::evmc_host_context,
-) -> ffi::evmc_tx_context {
-    let ctx = Box::from_raw(context as *mut HostContext);
-    println!("host context: {:?}", ctx);
-    std::mem::forget(ctx);
-    ffi::evmc_tx_context {
-        tx_gas_price: ffi::evmc_uint256be::default(),
-        tx_origin: ffi::evmc_address::default(),
-        block_coinbase: ffi::evmc_address::default(),
-        block_number: 0,
-        block_timestamp: 0,
-        block_gas_limit: 0,
-        block_difficulty: ffi::evmc_uint256be::default(),
-        chain_id: ffi::evmc_uint256be::default(),
+impl<T: HostContext> DerefMut for HostContextWrapper<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.inner.as_mut().unwrap()
+    }
+}
+
+pub trait HostContext {
+    fn interface() -> ffi::evmc_host_interface;
+
+    fn get_tx_context(&mut self) -> ffi::evmc_tx_context;
+    fn account_exists(&mut self, address: &Address) -> bool;
+    fn get_storage(&mut self, address: &Address, key: &Bytes32) -> Bytes32;
+    fn set_storage(&mut self, address: Address, key: Bytes32, value: Bytes32) -> StorageStatus;
+    fn get_balance(&mut self, address: &Address) -> Uint256;
+    fn call(&mut self, msg: ExecutionMessage) -> ExecutionResult;
+    fn selfdestruct(&mut self, address: &Address, beneficiary: &Address);
+    fn emit_log(&mut self, address: &Address, data: &[u8], topics: &[ffi::evmc_bytes32]);
+    fn copy_code(&mut self, address: &Address, code_offset: usize, buffer: &[u8]) -> usize;
+    fn get_code_size(&mut self, address: &Address) -> usize;
+    fn get_code_hash(&mut self, address: &Address) -> Bytes32;
+    fn get_block_hash(&mut self, number: u64) -> Bytes32;
+}
+
+pub fn get_interface<T: HostContext>() -> ffi::evmc_host_interface {
+    unsafe extern "C" fn account_exists<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        address: *const ffi::evmc_address,
+    ) -> bool {
+        let address = Address::from(*address);
+        HostContextWrapper::<T>::from(context).account_exists(&address)
+    }
+
+    unsafe extern "C" fn get_tx_context<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+    ) -> ffi::evmc_tx_context {
+        HostContextWrapper::<T>::from(context).get_tx_context()
+    }
+
+    unsafe extern "C" fn get_storage<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        address: *const ffi::evmc_address,
+        key: *const ffi::evmc_bytes32,
+    ) -> ffi::evmc_bytes32 {
+        let address = Address::from(*address);
+        let key = Bytes32::from(*key);
+        HostContextWrapper::<T>::from(context)
+            .get_storage(&address, &key)
+            .into()
+    }
+
+    unsafe extern "C" fn set_storage<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        address: *const ffi::evmc_address,
+        key: *const ffi::evmc_bytes32,
+        value: *const ffi::evmc_bytes32,
+    ) -> ffi::evmc_storage_status {
+        let address = Address::from(*address);
+        let key = Bytes32::from(*key);
+        let value = Bytes32::from(*value);
+        HostContextWrapper::<T>::from(context).set_storage(address, key, value)
+    }
+
+    unsafe extern "C" fn get_balance<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        address: *const ffi::evmc_address,
+    ) -> ffi::evmc_uint256be {
+        let address = Address::from(*address);
+        HostContextWrapper::<T>::from(context)
+            .get_balance(&address)
+            .into()
+    }
+
+    unsafe extern "C" fn call<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        msg: *const ffi::evmc_message,
+    ) -> ffi::evmc_result {
+        let message = ExecutionMessage::from(&*msg);
+        HostContextWrapper::<T>::from(context).call(message).into()
+    }
+
+    unsafe extern "C" fn emit_log<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        address: *const ffi::evmc_address,
+        data: *const u8,
+        data_size: usize,
+        topics: *const ffi::evmc_bytes32,
+        topics_count: usize,
+    ) {
+        let address = Address::from(*address);
+        let data: &[u8] = from_raw_parts(data, data_size);
+        let topics: &[ffi::evmc_bytes32] = from_raw_parts(topics, topics_count);
+        HostContextWrapper::<T>::from(context).emit_log(&address, data, topics)
+    }
+
+    unsafe extern "C" fn get_code_size<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        address: *const ffi::evmc_address,
+    ) -> usize {
+        let address = Address::from(*address);
+        HostContextWrapper::<T>::from(context).get_code_size(&address)
+    }
+
+    unsafe extern "C" fn get_code_hash<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        address: *const ffi::evmc_address,
+    ) -> ffi::evmc_bytes32 {
+        let address = Address::from(*address);
+        HostContextWrapper::<T>::from(context)
+            .get_code_hash(&address)
+            .into()
+    }
+
+    unsafe extern "C" fn get_block_hash<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        number: i64,
+    ) -> ffi::evmc_bytes32 {
+        HostContextWrapper::<T>::from(context)
+            .get_block_hash(number as u64)
+            .into()
+    }
+
+    unsafe extern "C" fn selfdestruct<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        address: *const ffi::evmc_address,
+        beneficiary: *const ffi::evmc_address,
+    ) {
+        let address = Address::from(*address);
+        let beneficiary = Address::from(*beneficiary);
+        HostContextWrapper::<T>::from(context).selfdestruct(&address, &beneficiary);
+    }
+
+    unsafe extern "C" fn copy_code<T: HostContext>(
+        context: *mut ffi::evmc_host_context,
+        address: *const ffi::evmc_address,
+        code_offset: usize,
+        buffer_data: *mut u8,
+        buffer_size: usize,
+    ) -> usize {
+        let address = Address::from(*address);
+        let buffer: &[u8] = from_raw_parts(buffer_data, buffer_size);
+        HostContextWrapper::<T>::from(context).copy_code(&address, code_offset, buffer)
+    }
+
+    ffi::evmc_host_interface {
+        get_tx_context: Some(get_tx_context::<T>),
+        account_exists: Some(account_exists::<T>),
+        get_storage: Some(get_storage::<T>),
+        set_storage: Some(set_storage::<T>),
+        get_balance: Some(get_balance::<T>),
+        selfdestruct: Some(selfdestruct::<T>),
+        call: Some(call::<T>),
+        emit_log: Some(emit_log::<T>),
+        copy_code: Some(copy_code::<T>),
+        get_code_size: Some(get_code_size::<T>),
+        get_code_hash: Some(get_code_hash::<T>),
+        get_block_hash: Some(get_block_hash::<T>),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,211 @@
-// use libc;
-use evmc_vm::ffi;
-
 mod evmone;
 
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+
+use evmc_sys as ffi;
 use evmone::{
-    get_dummy_tx_context, Evmone, ExecutionContext, ExecutionMessage, HostContext, HostContextPtr,
+    get_interface, Address, Bytes32, CallKind, Evmone, ExecutionContext, ExecutionMessage,
+    ExecutionResult, HostContext, HostContextPtr, Revision, StorageStatus, Uint256,
 };
 
+const SIMPLE_STORAGE_CODE: &str = "60806040525b607b60006000508190909055505b610018565b60db806100266000396000f3fe60806040526004361060295760003560e01c806360fe47b114602f5780636d4ce63c14605b576029565b60006000fd5b60596004803603602081101560445760006000fd5b81019080803590602001909291905050506084565b005b34801560675760006000fd5b50606e6094565b6040518082815260200191505060405180910390f35b8060006000508190909055505b50565b6000600060005054905060a2565b9056fea26469706673582212204e58804e375d4a732a7b67cce8d8ffa904fa534d4555e655a433ce0a5e0d339f64736f6c63430006060033";
+
 fn main() {
-    for i in 0..3 {
-        let interface = ffi::evmc_host_interface {
-            account_exists: None,
-            get_storage: None,
-            set_storage: None,
-            get_balance: None,
-            get_code_size: None,
-            get_code_hash: None,
-            copy_code: None,
-            selfdestruct: None,
-            call: None,
-            get_tx_context: Some(get_dummy_tx_context),
-            get_block_hash: None,
-            emit_log: None,
-        };
-        let host_context = Box::new(HostContext::default());
+    for i in 0..2 {
+        let host_context = Box::new(TestHostContext::new(0));
         let host_context_ptr = HostContextPtr::from(host_context);
-        let mut context = ExecutionContext::new(interface, host_context_ptr.ptr);
+        let mut context = ExecutionContext::new(TestHostContext::interface(), host_context_ptr.ptr);
         let instance = Evmone::new();
 
-        let destination = ffi::evmc_address { bytes: [32u8; 20] };
-        let sender = ffi::evmc_address { bytes: [128u8; 20] };
-        let value = ffi::evmc_uint256be { bytes: [0u8; 32] };
+        let destination = Address([32u8; 20]);
+        let sender = Address([128u8; 20]);
+        let value = Uint256([1u8; 32]);
         let create2_salt = ffi::evmc_bytes32 { bytes: [255u8; 32] };
+
+        let code = load_binary(&env::args().nth(1).unwrap());
+        let input_data = if let Some(input_data_path) = env::args().nth(2) {
+            load_binary(&input_data_path)
+        } else {
+            Vec::new()
+        };
+
         let raw_message = ffi::evmc_message {
-            kind: ffi::evmc_call_kind::EVMC_CALL,
+            kind: CallKind::EVMC_CREATE,
             flags: 44,
-            depth: 66,
-            gas: 4466,
-            destination,
-            sender,
-            input_data: std::ptr::null(),
-            input_size: 0,
-            value,
+            depth: 0,
+            gas: 4_466_666,
+            destination: destination.into(),
+            sender: sender.into(),
+            input_data: input_data.as_ptr(),
+            input_size: input_data.len(),
+            value: value.into(),
             create2_salt,
         };
-        let message = ExecutionMessage::from(raw_message);
-        let code = [0u8; 0];
-        let result = instance.execute(
-            ffi::evmc_revision::EVMC_PETERSBURG,
-            &code,
-            &message,
-            &mut context,
+        let message = ExecutionMessage::from(&raw_message);
+
+        let result = instance.execute(Revision::EVMC_PETERSBURG, &code, &message, &mut context);
+        println!("[Round {}] Execution result: {:?}\n", i, result);
+    }
+}
+
+fn load_binary(path: &str) -> Vec<u8> {
+    hex::decode(
+        String::from_utf8(fs::read(path).unwrap())
+            .unwrap()
+            .trim()
+            .as_bytes(),
+    )
+    .unwrap()
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
+pub struct Value {
+    data: Bytes32,
+    // Modified time:
+    //   0 => first set
+    //   1 => modified
+    //   2..n => modifled again
+    modified: usize,
+}
+
+impl Value {
+    fn new(data: Bytes32) -> Value {
+        Value { data, modified: 0 }
+    }
+
+    fn update_data(&mut self, data: Bytes32) -> bool {
+        if data != self.data {
+            self.data = data;
+            self.modified += 1;
+            true
+        } else {
+            false
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct TestHostContext {
+    pub depth: u32,
+    pub code: Vec<u8>,
+    pub storage: HashMap<Address, HashMap<Bytes32, Value>>,
+}
+
+impl TestHostContext {
+    pub fn new(depth: u32) -> TestHostContext {
+        TestHostContext {
+            depth,
+            code: Vec::new(),
+            storage: HashMap::default(),
+        }
+    }
+}
+
+impl HostContext for TestHostContext {
+    fn interface() -> ffi::evmc_host_interface {
+        get_interface::<TestHostContext>()
+    }
+
+    fn get_tx_context(&mut self) -> ffi::evmc_tx_context {
+        ffi::evmc_tx_context {
+            tx_gas_price: Uint256::default().into(),
+            tx_origin: Address::default().into(),
+            block_coinbase: Address::default().into(),
+            block_number: 0,
+            block_timestamp: 0,
+            block_gas_limit: 666_666_666,
+            block_difficulty: Uint256::default().into(),
+            chain_id: Uint256::default().into(),
+        }
+    }
+
+    fn account_exists(&mut self, address: &Address) -> bool {
+        println!("account_exists(address: {:?})", address);
+        true
+    }
+
+    fn get_storage(&mut self, address: &Address, key: &Bytes32) -> Bytes32 {
+        println!("get(address: {:?}, key: {:?})", address, key);
+        self.storage
+            .get(address)
+            .and_then(|map| map.get(key))
+            .map(|value| value.data.clone())
+            .unwrap_or_default()
+    }
+
+    fn set_storage(&mut self, address: Address, key: Bytes32, value: Bytes32) -> StorageStatus {
+        println!(
+            "set(address: {:?}, key: {:?}), value: {:?}",
+            address, key, value
         );
-        println!("[Round {}] Execution result: {:?}", i, result);
+        let val = self
+            .storage
+            .entry(address)
+            .or_default()
+            .entry(key)
+            .or_insert_with(|| Value::new(value.clone()));
+        let changed = val.update_data(value);
+
+        match (val.modified, changed) {
+            (0, true) => unreachable!(),
+            (0, false) => StorageStatus::EVMC_STORAGE_ADDED,
+            (1, true) => StorageStatus::EVMC_STORAGE_MODIFIED,
+            (_, true) => StorageStatus::EVMC_STORAGE_MODIFIED_AGAIN,
+            (_, false) => StorageStatus::EVMC_STORAGE_UNCHANGED,
+        }
+    }
+
+    fn get_balance(&mut self, address: &Address) -> Uint256 {
+        println!("get_balance(address: {:?})", address);
+        Uint256::default()
+    }
+
+    fn call(&mut self, message: ExecutionMessage) -> ExecutionResult {
+        println!("call(message: {:?})", message);
+        let code = hex::decode(&SIMPLE_STORAGE_CODE).unwrap();
+        let host_context = Box::new(TestHostContext::new(message.depth as u32 + 1));
+        let host_context_ptr = HostContextPtr::from(host_context);
+        let mut context = ExecutionContext::new(TestHostContext::interface(), host_context_ptr.ptr);
+        let instance = Evmone::new();
+        instance.execute(Revision::EVMC_PETERSBURG, &code, &message, &mut context)
+    }
+
+    fn selfdestruct(&mut self, address: &Address, beneficiary: &Address) {
+        println!(
+            "emit_log(address: {:?}, beneficiary: {:?})",
+            address, beneficiary
+        );
+    }
+
+    fn emit_log(&mut self, address: &Address, data: &[u8], topics: &[ffi::evmc_bytes32]) {
+        println!(
+            "emit_log(address: {:?}, data: {:?}, topics: {:?})",
+            address, data, topics
+        );
+    }
+
+    fn copy_code(&mut self, address: &Address, code_offset: usize, buffer: &[u8]) -> usize {
+        println!(
+            "copy_code(address: {:?}, code_offset: {:?}, buffer: {:?})",
+            address, code_offset, buffer
+        );
+        0
+    }
+
+    fn get_code_size(&mut self, address: &Address) -> usize {
+        println!("get_code_size(address: {:?})", address);
+        0
+    }
+
+    fn get_code_hash(&mut self, address: &Address) -> Bytes32 {
+        println!("get_code_hash(address: {:?})", address);
+        Bytes32::default()
+    }
+
+    fn get_block_hash(&mut self, number: u64) -> Bytes32 {
+        println!("get_block_hash(number: {:?})", number);
+        Bytes32::default()
     }
 }
